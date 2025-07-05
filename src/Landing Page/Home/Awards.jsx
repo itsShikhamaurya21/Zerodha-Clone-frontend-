50b13916f660861f0f42ef3b16f808f32231572b
Shikha <shikhamaurya987373@gmail.com>
2025-07-05 12:32:26 +0530

    Component-Structure-Setup

---
+++ src/Landing Page/Home/Awards.jsx
@@ -0,0 +1,8 @@
+function Awards() {
+  return (
+    <>
+      <h1>Awards</h1>
+    </>
+  );
+}
+export default Awards;
---
+++ src/Landing Page/Home/Education.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+function Education() {
+  return <>Education</>;
+}
+
+export default Education;
